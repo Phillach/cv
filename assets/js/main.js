99523e7bffc1fc85fec217d12c77e9ed55f63613
Philippe Lachance <lachance92@gmail.com>
2016-10-30 16:38:54 -0400

    fixed modal css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
-window.addEventListener('hashchange', function(event){
-    var modalSkills = document.querySelector('.modal-skills');
-    var isOpen = document.querySelector('.is-open');
-    var skillsLink = document.querySelector('.skills-link');
-    var currentLink = document.querySelector('.current-link');
-    var closeBtn = document.querySelector('.close-btn');
-
-    if (currentLink) {
-        currentLink.classList.remove('current-link');
-    }
-
-    if (isOpen) {
-        isOpen.classList.remove('is-open');
-    }
-
-    switch (window.location.hash) {
-        case "#skills":
-            modalSkills.classList.add('is-open');
-            skillsLink.classList.add('current-link');
-
-            break;
-    }
-});
+// window.addEventListener('hashchange', function(event){
+//     var modalSkills = document.querySelector('.modal-skills');
+//     var isOpen = document.querySelector('.is-open');
+//     var skillsLink = document.querySelector('.skills-link');
+//     var currentLink = document.querySelector('.current-link');
+//     var closeBtn = document.querySelector('.close-btn');
+//
+//     if (currentLink) {
+//         currentLink.classList.remove('current-link');
+//     }
+//
+//     if (isOpen) {
+//         isOpen.classList.remove('is-open');
+//     }
+//
+//     switch (window.location.hash) {
+//         case "#skills":
+//             modalSkills.classList.add('is-open');
+//             skillsLink.classList.add('current-link');
+//
+//             break;
+//     }
+// });
