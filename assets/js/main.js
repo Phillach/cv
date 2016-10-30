6e976af5d31fead05fa56a1bd8ab6d11df36bd88
Philippe Lachance <lachance92@gmail.com>
2016-10-30 13:11:28 -0400

    Im sorry jQuery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,28 @@
-window.addEventListener("hashchange", function(event){
-    var modalWrapperFormation = document.querySelector('.modal-wrapper-formation');
-    var modalWrapperRealisation = document.querySelector('.modal-wrapper-realisation');
-    var currentlyOpen = document.querySelector(".is-open");
-    var formationLink = document.querySelector(".formation-link");
-    var realisationLink = document.querySelector(".realisation-link");
-    var currentLink = document.querySelector(".current-link");
-
-    if (currentLink) {
-        currentLink.classList.remove("current-link");
-    }
-
-    if (currentlyOpen) {
-        currentlyOpen.classList.remove('is-open');
-    }
-
-    switch (window.location.hash) {
-        case "#formation":
-            modalWrapperFormation.classList.add("is-open");
-            formationLink.classList.add("current-link");
-            
-            break;
-
-        case "#realisation":
-            modalWrapperRealisation.classList.add("is-open");
-            realisationLink.classList.add("current-link");
-
-            break;
-    }
-});
+// window.addEventListener("hashchange", function(event){
+//     var modalSkills = document.querySelector('.modal-skills');
+//     var currentlyOpen = document.querySelector(".is-open");
+//     var skillsLink = document.querySelector(".skills-link");
+//     var currentLink = document.querySelector(".current-link");
+//
+//     if (currentLink) {
+//         currentLink.classList.remove("current-link");
+//     }
+//
+//     if (currentlyOpen) {
+//         currentlyOpen.classList.remove('is-open');
+//     }
+//
+//     switch (window.location.hash) {
+//         case "#skills":
+//             modalSkills.classList.add("is-open");
+//             skillsLink.classList.add("current-link");
+//
+//             break;
+//
+//         case "#reel":
+//             modalWrapperRealisation.classList.add("is-open");
+//             realisationLink.classList.add("current-link");
+//
+//             break;
+//     }
+// });
